8997e4491c6711f7742bf9aa2b7fbac372008e40
sheldongohetia <sheldongohetia.ca@gmail.com>
2025-01-30 02:41:55 -0500

    Updated the design
    In this commit, added a style on the page, additional characters and adjustment of the API for images.

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ This repo is for our HW#1 AJAX REDUX - which we create a Star Wars page integrat
 
 ## Installation
 
-Click on the link to the sample repository provided;
-On the repo, click on the green button "< > Code";
-On the last option, click on "Download zip";
-Unzip the folder.
+1. Click on the link to the sample repository provided;
+2. On the repo, click on the green button "< > Code";
+3. On the last option, click on "Download zip";
+4. Unzip the folder.
 
 ## Usage
 
-Download the folder on your device;
-Open the index.html file on the browser of your choice.
+1. Download the folder on your device;
+2. Open the index.html file on the browser of your choice.
 
 ## History
 
--- js/main.js
+++ js/main.js
@@ -1,116 +1,234 @@
 (() => {
-  const characterBox = document.querySelector("#character-box");
-  const detailsTemplate = document.querySelector("#details-template");
-  const detailsCon = document.querySelector("#details-con");
-  const filmTemplate = document.querySelector("#film-template");
-  const baseURL = "https://swapi.dev/api/people/?page=";
-  const totalPages = 9;
+  const charBox = document.querySelector("#char-select-box");
+  const movieTemplate = document.querySelector("#movie-template");
+  const movieCon = document.querySelector("#movie-con");
+  const charPreview = document.querySelector("#char-preview");
+  let charPreviewOn = false;
 
-  function getAllCharacters() {
-    const allChar = [];
+  let tl = gsap.timeline();
 
-    for (let i = 1; i <= totalPages; i++) {
-      allChar.push(fetch(`${baseURL}${i}`).then((response) => response.json()));
+  const baseUrl = `https://swapi.dev/api/`;
+
+  function getCharacters() {
+    charBox.classList.add("spinner");
+
+    // Fetch characters from page 1
+    fetch(`${baseUrl}/people`)
+      .then((response) => response.json())
+      .then(function (response) {
+        console.log("Page 1 Characters:", response.results);
+
+        const characters = response.results;
+
+        // Loop through the characters from page 1
+        characters.forEach((character, index) => {
+          const div = document.createElement("div");
+          const p = document.createElement("p");
+          const img = document.createElement("img");
+          const a = document.createElement("a");
+
+          p.textContent = character.name;
+          img.src = `images/avatars/${index + 1}.jpg`;
+          a.classList.add("character-frame");
+          a.appendChild(img);
+          a.appendChild(p);
+          let randFilmIndex = Math.floor(
+            Math.random() * character.films.length
+          );
+          let randFilm = character.films[randFilmIndex];
+          a.dataset.movieDetails = randFilm;
+          a.dataset.charNumber = index;
+          div.appendChild(a);
+          div.classList.add("character");
+          charBox.appendChild(div);
+        });
+
+        // Fetch characters 11 and 12 (instead of page 2)
+        return Promise.all([
+          fetch(`${baseUrl}/people/11/`),
+          fetch(`${baseUrl}/people/14/`),
+        ]);
+      })
+      .then(([response11, response12]) =>
+        Promise.all([response11.json(), response12.json()])
+      )
+      .then(function ([character11, character12]) {
+        console.log("Character 11:", character11);
+        console.log("Character 12:", character12);
+
+        const div11 = document.createElement("div");
+        const p11 = document.createElement("p");
+        const img11 = document.createElement("img");
+        const a11 = document.createElement("a");
+
+        p11.textContent = character11.name;
+        img11.src = `images/avatars/11.jpg`; // Added image for the specific character
+        a11.classList.add("character-frame");
+        a11.appendChild(img11);
+        a11.appendChild(p11);
+        let randFilmIndex11 = Math.floor(
+          Math.random() * character11.films.length
+        );
+        let randFilm11 = character11.films[randFilmIndex11];
+        a11.dataset.movieDetails = randFilm11;
+        a11.dataset.charNumber = 10;
+        div11.appendChild(a11);
+        div11.classList.add("character");
+        charBox.appendChild(div11);
+
+        // Append character 12
+        const div12 = document.createElement("div");
+        const p12 = document.createElement("p");
+        const img12 = document.createElement("img");
+        const a12 = document.createElement("a");
+
+        p12.textContent = character12.name;
+        img12.src = `images/avatars/12.jpg`; // Added image for the specific character
+        a12.classList.add("character-frame");
+        a12.appendChild(img12);
+        a12.appendChild(p12);
+        let randFilmIndex12 = Math.floor(
+          Math.random() * character12.films.length
+        );
+        let randFilm12 = character12.films[randFilmIndex12];
+        a12.dataset.movieDetails = randFilm12;
+        a12.dataset.charNumber = 11;
+        div12.appendChild(a12);
+        div12.classList.add("character");
+        charBox.appendChild(div12);
+      })
+      .then(function () {
+        const links = document.querySelectorAll("#char-select-box div a");
+        links.forEach((link) => {
+          link.addEventListener("click", getMovieInfo);
+        });
+      })
+      .catch((err) => {
+        console.log("Error fetching characters:", err);
+      })
+      .finally(() => {
+        charBox.classList.remove("spinner");
+      });
+  }
+
+  function getMovieInfo() {
+    movieCon.innerHTML = "";
+    movieCon.classList.add("spinner");
+
+    const movieID = this.dataset.movieDetails;
+    const charNumber = this.dataset.charNumber;
+
+    const div = document.querySelector(".img-holder");
+    div.classList.add("invisible");
+    const img = document.createElement("img");
+
+    if (charPreviewOn) {
+      tl.set(div, { autoAlpha: 0 });
+      div.innerHTML = "";
     }
 
-    Promise.all(allChar)
-      .then((responses) => {
-        const allCharacters = responses.flatMap((response) => response.results);
+    img.src = `images/preview/${charNumber}.png`;
+    div.appendChild(img);
+    charPreviewOn = true;
+    charPreview.appendChild(div);
 
-        const ul = document.createElement("ul");
-        allCharacters.forEach((character) => {
-          const li = document.createElement("li");
-          const a = document.createElement("a");
-          a.textContent = character.name;
-          a.dataset.url = character.url;
-          li.appendChild(a);
-          ul.appendChild(li);
-        });
-        characterBox.appendChild(ul);
-      })
-      .then(() => {
-        const links = document.querySelectorAll("#character-box li a");
-        links.forEach((link) => {
-          link.addEventListener("click", getCharacterDetails);
-        });
-      })
-      .catch((err) => {
-        console.error("Failed to fetch characters:", err);
-      });
-  }
+    tl.to(div, { autoAlpha: 1, duration: 2, ease: "power2.inOut" });
 
-  function getCharacterDetails(e) {
-    const characterURL = e.currentTarget.dataset.url;
-    fetch(characterURL)
+    fetch(movieID)
       .then((response) => response.json())
-      .then((character) => {
-        detailsCon.innerHTML = "";
+      .then(function (response) {
+        movieCon.classList.remove("spinner");
+        movieCon.innerHTML = "";
+        const template = document.importNode(movieTemplate.content, true);
+        const movieBody = template.querySelector(".opening-crawl");
+        const movieTitle = template.querySelector(".movie-title");
+        const posterImg = template.querySelector(".poster-img");
+        posterImg.src = `images/movies/${response.episode_id}.jpg`;
+        movieTitle.textContent = response.title;
+        movieBody.innerHTML = response.opening_crawl;
 
-        const clone = detailsTemplate.content.cloneNode(true);
-        const detailsName = clone.querySelector(".details-name");
-        const detailsGender = clone.querySelector(".details-gender");
-        const detailsBirth = clone.querySelector(".details-birth");
-        const detailsFilms = clone.querySelector(".details-films");
-
-        detailsName.textContent = `Name: ${character.name}`;
-        detailsGender.textContent = `Gender: ${character.gender}`;
-        detailsBirth.textContent = `Birth Year: ${character.birth_year}`;
-
-        const filmPromises = character.films.map((filmURL) =>
-          fetch(filmURL).then((res) => res.json())
-        );
-
-        Promise.all(filmPromises)
-          .then((films) => {
-            films.forEach((film) => {
-              const filmLi = document.createElement("li");
-              const filmLink = document.createElement("a");
-              filmLink.textContent = film.title;
-              filmLink.href = "#";
-              filmLink.dataset.url = film.url;
-              filmLink.addEventListener("click", getFilmDetails);
-              filmLi.appendChild(filmLink);
-              detailsFilms.appendChild(filmLi);
-            });
-          })
-          .catch((err) => {
-            console.error("Failed to fetch film details:", err);
-          });
-
-        detailsCon.appendChild(clone);
+        movieCon.appendChild(template);
       })
-      .catch((err) => {
-        detailsCon.innerHTML =
-          "<p>Failed to fetch character details. Try again!</p>";
-        console.error("Error fetching character details:", err);
+      .catch((error) => {
+        console.log(error);
       });
   }
 
-  function getFilmDetails(e) {
-    e.preventDefault();
-    const filmURL = e.currentTarget.dataset.url;
-    fetch(filmURL)
-      .then((response) => response.json())
-      .then((film) => {
-        detailsCon.innerHTML = "";
+  getCharacters();
 
-        const clone = filmTemplate.content.cloneNode(true);
-        const filmTitle = clone.querySelector(".film-title");
-        const filmDirector = clone.querySelector(".film-director");
-        const filmRelease = clone.querySelector(".film-release");
-
-        filmTitle.textContent = `Title: ${film.title}`;
-        filmDirector.textContent = `Director: ${film.director}`;
-        filmRelease.textContent = `Release Date: ${film.release_date}`;
-
-        detailsCon.appendChild(clone);
-      })
-      .catch((err) => {
-        detailsCon.innerHTML =
-          "<p>Failed to fetch film details. Try again!</p>";
-        console.error("Error fetching film details:", err);
-      });
-  }
-
-  getAllCharacters();
+  particlesJS("particles", {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#ffffff",
+        },
+      },
+      opacity: {
+        value: 0.8,
+        random: true,
+        animation: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+      },
+    },
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onHover: {
+          enable: true,
+          mode: "push",
+        },
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        repulse: {
+          distance: 100,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+      },
+    },
+    retina_detect: true,
+  });
 })();
